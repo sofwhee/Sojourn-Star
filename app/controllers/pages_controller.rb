37ab85d3ae523df6a816876a5e2fc122fa2d1329
sofwhee <sofiamsola@outlook.com>
2024-06-20 17:55:56 -0400

    fill in pages model controller and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,24 @@
 class PagesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+  
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  private
+  def page_params
+    permit.require(:page).permit(:chapter, :page, :image)
+  end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,5 @@
 class Page < ApplicationRecord
+
+  validates :chapter, presence: true
+  validates :page, presence: true, uniqueness: true
 end
