b6cc7e747d5af16a2c0006b36615094e071e3849
sofwhee <sofiamsola@outlook.com>
2024-06-21 11:45:26 -0400

    change links on index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_20_182010) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_20_214735) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_20_182010) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.integer "chapter"
+    t.integer "page"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
