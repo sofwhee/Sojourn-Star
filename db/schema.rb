daf74025cada118144d22d5a0db5055cac181c8f
sofwhee <sofiamsola@outlook.com>
2024-07-28 20:40:33 -0400

    remove chapter column from pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_29_003059) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_29_003957) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,7 +61,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_29_003059) do
   end
 
   create_table "pages", force: :cascade do |t|
-    t.integer "chapter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "page_number"
