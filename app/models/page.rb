cca5ea3427154fda7ced21f1f4fe303750785a88
sofwhee <sofiamsola@outlook.com>
2024-07-28 20:34:24 -0400

    add chapter model

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,11 +2,17 @@ class Page < ApplicationRecord
   has_one_attached :page_image
 
   validates :chapter, :page_number, :page_image, presence: true
+  validates :page_number, uniqueness: true
 
-  scope :sorted, -> { order(published_at: :asc) }
+  scope :sorted, -> { order(chapter: :asc, page_number: :asc) }
+  scope :grouped, -> { select() }
   scope :draft, -> { where(published_at: nil) }
   scope :published, -> { where("published_at <= ?", Time.current) }
   scope :scheduled, -> { where("published_at > ?", Time.current) }
+
+  def sorted
+    sorted.group
+  end
 
   def draft?
     published_at.nil?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_21_233847) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_29_003059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_21_233847) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "chapters", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pages", force: :cascade do |t|
