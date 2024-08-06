16d47ed9a490e8ce70f2c6bdcfb0cbdba7e5bd7d
sofwhee <sofiamsola@outlook.com>
2024-08-06 12:41:25 -0400

    migrate chapter_number to chapters table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_01_212911) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_06_164020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_01_212911) do
 
   create_table "chapters", force: :cascade do |t|
     t.string "name"
+    t.integer "chapter_number"
   end
 
   create_table "pages", force: :cascade do |t|
