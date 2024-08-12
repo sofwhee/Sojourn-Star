215eacd786e128298e4a8be0e8f89ca08472800d
sofwhee <sofiamsola@outlook.com>
2024-08-12 13:09:58 -0400

    center comic page using flexbox on columns

---
+++ app/javascript/home.js
@@ -0,0 +1,10 @@
+const cpageimg = document.getElementById("cpage").firstElementChild
+const leftColumn = document.getElementById("leftcolumn")
+
+function columnsmaxflexwidth()
+let comicWidth = cpageimg.clientWidth
+leftColumn.style.maxWidth = ""
+
+
+
+// apply styling
