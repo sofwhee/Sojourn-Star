5806c4b7075f3f51529325f486c55af274273a7b
sofwhee <sofiamsola@outlook.com>
2024-08-15 15:01:05 -0400

    allow for linking single css file at a time

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets/pages .css
+//= link_directory ../stylesheets/archive .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
