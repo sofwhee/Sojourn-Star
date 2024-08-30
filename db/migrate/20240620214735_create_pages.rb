ef9581fe6107d6a72c308813e1afa83e0087694d
sofwhee <sofiamsola@outlook.com>
2024-08-30 11:26:32 -0400

    change migration

--- db/migrate/20240620214735_create_pages.rb
+++ db/migrate/20240620214735_create_pages.rb
@@ -3,7 +3,6 @@ class CreatePages < ActiveRecord::Migration[7.1]
     create_table :pages do |t|
       t.integer :page
       t.string :image
-      t.belongs_to :chapter, index: true, foreign_key: true
 
       t.timestamps
     end
